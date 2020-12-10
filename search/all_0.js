b0a2bbbe5bd0e7b7ddb1adedb05f08e45003f419
llandau <leo_landau@yahoo.com>
2020-12-10 15:27:29 -0800

    update project docs

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['noetherad_0',['NoetherAD',['../class_noether_a_d.html',1,'NoetherAD'],['../class_noether_a_d.html#aaac7f4bb24d8576944e66cbf69ae8892',1,'NoetherAD::NoetherAD()']]]
+];
---
+++ search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['noetherad_1',['NoetherAD',['../class_noether_a_d.html',1,'']]]
+];
